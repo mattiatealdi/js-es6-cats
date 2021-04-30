f991ef9dd452ba144de0d3cea9746de274be8430
mattiatealdi <mattiatealdi@gmail.com>
2021-04-30 18:29:43 +0200

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,27 @@ $(document).ready(function(){
   let cats = [
       {
       nome: "Stella",
-      età: 2,
-      colore: "nero",
-      sesso: 'F'
+      age: 2,
+      colore: "yellow",
+      gender: 'F'
     },
     {
       nome: "Felix",
-      età: 7,
-      colore: "bianco",
-      sesso: 'M'
+      age: 7,
+      colore: "white",
+      gender: 'M'
     },
     {
       nome: "Vinny",
-      età: 13,
-      colore: "marrone",
-      sesso: 'F'
+      age: 13,
+      colore: "brown",
+      gender: 'F'
     },
     {
       nome: "Stella",
-      età: 2,
-      colore: "nero",
-      sesso: 'M'
+      age: 2,
+      colore: "black",
+      gender: 'M'
     }
   ]
 
@@ -33,17 +33,57 @@ cats.forEach((cat)=>{
 
 });
 
+const pink = '#FF00FF';
+const blue = "#0099FF";
+const newCats = cats.map((cat) =>{
+  let color;
+  if(cat.gender =='F'){
+    color = pink;
+  }else{
+    color = blue;
+  }
+  let opacity = cat.age / 10;
 
-function listGenerator(catColor, name){
+  return {
+    ...cat,
+    ribbon:{
+      color,
+      opacity
+    }
+  }
 
-  let html =  `
+});
+
+const femaleCats = newCats.filter((cat) => cat.gender == 'F');
+const maleCats = newCats.filter((cat) => cat.gender == 'M');
+
+femaleCats.forEach((cat)=>{
+
+  $('#female_cats ul').append(listGenerator(cat.colore, cat.nome, cat.ribbon.color, cat.ribbon.opacity));
+
+});
+
+maleCats.forEach((cat)=>{
+
+  $('#male_cats ul').append(listGenerator(cat.colore, cat.nome, cat.ribbon.color, cat.ribbon.opacity));
+
+});
+
+function listGenerator(catColor, name, ...ribbon){
+
+  let ribbonTag= "";
+  if(ribbon.length > 0){
+    ribbonTag = `<i class="fas fa-ribbon" style="color:${ribbon[0]}; opacity:${ribbon[1]}"></i>`;
+  };
+  let html = `
   <li>
-    <i class="fas fa-cat" style="color:$(catColor)"></i>
-    <span>$(name)</span>
+    <i class="fas fa-cat" style="color:${catColor}"></i>
+    ${ribbonTag}
+    <span>${name}</span>
   </li>
   `;
   return html;
-}
+};
 
 
 });
